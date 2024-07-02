8893cbd1706ee09198b70d5bb322f28af3de25e4
Abhishek <s.abhishek7070@gmail.com>
2024-07-02 11:08:58 +0530

    first commit

---
+++ user-FE/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Footer from './Footer'
+
+const Header = () => {
+  return (
+    <div className='head text-white text-3xl md:text-5xl font-bold bg-slate-800 p-5 md:py-10 md:px-6 '>Pankaj FinanceGaurd Solutions.</div>
+    
+  )
+}
+
+export default Header

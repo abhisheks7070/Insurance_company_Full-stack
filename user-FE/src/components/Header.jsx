cc871369777c2f66a32bb2bc0578f96597eb5cfa
Abhishek <s.abhishek7070@gmail.com>
2024-07-03 11:38:32 +0530

    modified

--- user-FE/src/components/Header.jsx
+++ user-FE/src/components/Header.jsx
@@ -10,11 +10,11 @@ const [correct, setCorrect] = useState("")
   return (<div className='flex flex-col gap-4 md:flex-row justify-between items-start md:items-center bg-slate-800 p-5 md:py-10 md:px-6'>
     <div className='head text-white text-3xl md:text-5xl font-bold  '>Pankaj FinanceGaurd Solutions.</div>
     <div className='text-xs md:text-xl'>
-      <span className='mr-1'>
-        <input type="text" value={correct} onChange={(e)=>setCorrect(e.target.value)} className='py-1 px-3 rounded-xl text-black' placeholder='Enter Agent website code' />
+      <span className='mr-2'>
+        <input type="text" value={correct} onChange={(e)=>setCorrect(e.target.value)} className='py-1 px-3 rounded-xl text-black  w-[50vw] md:w-auto' placeholder='Enter Agent website code' />
       </span>
-      <span className='p-1 text-black'>
-        {correct=="Abhishek"? (<a className='px-1.5 bg-green-500 rounded-xl' href="https://ps-agent.onrender.com/" target="_blank">Click</a>) : (<span className='bg-red-600 px-1.5 rounded-xl'>Incorrect</span>)}</span>
+      <span className='text-black'>
+        {correct=="Abhishek"? (<a className=' border-3 border-solid border-black px-1.5 bg-green-500 rounded-xl' href="https://ps-agent.onrender.com/" target="_blank">Click</a>) : (<span className='border-3 border-solid border-black bg-red-600 px-1.5 rounded-xl'>Incorrect</span>)}</span>
     </div>
   </div>
     

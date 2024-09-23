d96b98ef64dbef8a1d75c8568cfee4afa305969c
Abhishek <s.abhishek7070@gmail.com>
2024-09-23 16:27:04 +0530

    header modified

--- user-FE/src/components/Header.jsx
+++ user-FE/src/components/Header.jsx
@@ -10,8 +10,8 @@ const Header = () => {
 
   return (
   <>
-  <div className='header h-[17vh] md:h-[40vh] w-[100vw]'>
-    <img className='object-fill h-[17vh] md:h-[40vh] w-[100vw]' src= {header} alt="" />
+  <div className='header h-[17vh] lg:h-[40vh] w-[100vw]'>
+    <img className='object-fill h-[17vh] lg:h-[40vh] w-[100vw]' src= {header} alt="" />
     {/* <div className='head text-white text-3xl md:text-5xl font-bold  '>Pankaj FinanceGaurd Solutions.</div> */}
     </div>
     <div>
@@ -21,7 +21,7 @@ const Header = () => {
         <input type="text" value={correct} onChange={(e) => setCorrect(e.target.value)} className='placeholder-black py-1 px-3 rounded-xl text-black w-auto' placeholder='Enter Agent website code' />
       </div>
       <div className='text-black py-3'>
-        {(correct.trim()) == "Pankaj_Singh" && (<a className=' border-3 border-solid border-black px-1.5 bg-green-500 rounded-xl' href="https://ps-agent.onrender.com/" target="_blank">Click</a>)}</div>
+        {(correct.trim()) == "Pankaj_Sinlg" && (<a className=' border-3 border-solid border-black px-1.5 bg-green-500 rounded-xl' href="https://ps-agent.onrender.com/" target="_blank">Click</a>)}</div>
     </div>
   </div>
   </>
